3b57a36740252751632cfe9bd0eb18b372378a53
vinodh1988 <k.c.vinodh@hotmail.com>
2025-06-26 11:55:53 +0530

    latest

---
+++ src/main/java/com/solution/ActivemqListenerMicroroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqListenerMicroroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqListenerMicroroserviceApplication.class, args);
+	}
+
+}
